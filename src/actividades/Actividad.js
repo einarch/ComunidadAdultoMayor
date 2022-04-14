0dc46aed55df3824e97449f32840f11839080556
rodrigo.gutierrez <rodrigo.gutierrez.acc@gmail.com>
2022-04-14 11:22:45 -0400

    Fixed section color

--- src/actividades/Actividad.js
+++ src/actividades/Actividad.js
@@ -34,24 +34,23 @@ const Actividad = ({ children }) => {
                 <br />
                 <br />
                 <br />
-                <br />
                 <h1 className="header">Actividades y Eventos</h1 >
                 <br />
                 <br />
-                <Container id="containerSection" className="bg-light p-4 mb-4">
+                <Container id="containerSection" className="p-4 mb-4">
                     {data.map(actividad => {
                         return (
-                            <div id="itemSectionCard" class="card shadow-0 border rounded-3 p-3 mb-2">
+                            <div id="itemSectionCard" className="card shadow-0 border rounded-3 p-3 mb-2">
                                 <div class="card-body">
                                     <div class="media d-flex">
                                         <img src={avatar} alt="" class="rounded-circle" height="80" width="80"></img>
                                         <div class="media-body flex-grow-1 ms-3">
-                                            <h5 class="mt-0 mb-1" className='activityUserName'><b>{actividad.nombreusuario}</b></h5>
-                                            <h6 class="media-heading"><b>Actividad:</b> <span className='activityTitle'>{actividad.actividad}</span></h6>
-                                            <h6 class="media-heading"><b>Fecha y hora:</b> {dateFormat(actividad.fechahora, "dd/mm/yyyy h:MM TT")}</h6>
-                                            <h6 class="media-heading"><b>Ubicación:</b> {actividad.ubicacion}</h6>
-                                            <h6 class="media-heading">{actividad.descripcion}</h6>
-                                            <h6 class="media-heading"><b>{actividad.numusuarios}</b> personas asistirán a la actividad.</h6>
+                                            <h5 className='activityUserName mt-0 mb-1'><b>{actividad.nombreusuario}</b></h5>
+                                            <h6 className="media-heading"><b>Actividad:</b> <span className='activityTitle'>{actividad.actividad}</span></h6>
+                                            <h6 className="media-heading"><b>Fecha y hora:</b> {dateFormat(actividad.fechahora, "dd/mm/yyyy h:MM TT")}</h6>
+                                            <h6 className="media-heading"><b>Ubicación:</b> {actividad.ubicacion}</h6>
+                                            <h6 className="media-heading">{actividad.descripcion}</h6>
+                                            <h6 className="media-heading"><b>{actividad.numusuarios}</b> personas asistirán a la actividad.</h6>
                                         </div>
                                     </div>
                                 </div>
--- src/apoyo/Apoyo.js
+++ src/apoyo/Apoyo.js
@@ -33,23 +33,22 @@ const Apoyo = ({ children }) => {
                 <br />
                 <br />
                 <br />
-                <br />
                 <h1 className="header"> Voluntarios de Apoyo</h1>
                 <br />
                 <br />
-                <Container id="containerSection" className="bg-light p-4 mb-4">
+                <Container id="containerSection" className="p-4 mb-4">
                     {data.map(apoyo => {
                         return (
-                            <div id="itemSectionCard" class="card shadow-0 border rounded-3 p-3 mb-2">
-                                <div class="card-body">
+                            <div id="itemSectionCard" className="card shadow-0 border rounded-3 p-3 mb-2">
+                                <div className="card-body">
                                     <div class="media d-flex">
                                         <img src={avatar} alt="" class="rounded-circle" height="80" width="80"></img>
-                                        <div class="media-body flex-grow-1 ms-3">
-                                            <h5 class="mt-0 mb-1" className='voluntarioUserName'><b>{apoyo.nombre}</b></h5>
+                                        <div className="media-body flex-grow-1 ms-3">
+                                            <h5 className='voluntarioUserName mt-0 mb-1'><b>{apoyo.nombre}</b></h5>
                                             <br></br>
-                                            <h6 class="media-heading">{apoyo.ciudad} &emsp; &ensp; <b>Teléfono:</b>{apoyo.telefono} </h6>
+                                            <h6 className="media-heading">{apoyo.ciudad} &emsp; &ensp; <b>Teléfono:</b>{apoyo.telefono} </h6>
                                             <br></br>
-                                            <h6 class="media-heading"><b>Días Disponibles:</b> {apoyo.diasdisp}</h6>
+                                            <h6 className="media-heading"><b>Días Disponibles:</b> {apoyo.diasdisp}</h6>
                                             <br></br>
                                         </div>
                                     </div>
