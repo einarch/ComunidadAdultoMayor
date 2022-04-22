eccc623f3ca401fde79eb9ce1124724437ffa2ec
Jose Miguel <josemiguelin7@gmail.com>
2022-04-22 19:56:20 -0400

    cambios de diseños en la descripion

--- src/components/DescripcionApp.js
+++ src/components/DescripcionApp.js
@@ -7,9 +7,7 @@ function DescripcionApp() {
 
         <Container className="descripcionApp">
         <br></br>
-        <br></br>
             CAM es una comunidad donde los Adultos Mayores pueden expresarse, encontrar actividades de su interés y solicitar algún tipo de apoyo de gente voluntaria. Porque ser parte de una comunidad hace la vida mas rica y divertida.
-        <br></br>
         <br></br>
         <br></br>
         </Container>
