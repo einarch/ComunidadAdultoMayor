18c2070efa1cd2ca2dd45d904aae21cb2ee51c59
rodrigo.gutierrez <rodrigo.gutierrez.acc@gmail.com>
2022-04-22 14:31:44 -0400

    Removed PODRAS word and added app description in main page

---
+++ src/components/DescripcionApp.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Container } from 'react-bootstrap';
+import './DescripcionApp.css';
+
+function DescripcionApp() {
+    return (
+
+        <Container className="descripcionApp">
+        <br></br>
+        <br></br>
+            CAM es una comunidad donde los Adultos Mayores pueden expresarse, encontrar actividades de su interés y solicitar algún tipo de apoyo de gente voluntaria. Porque ser parte de una comunidad hace la vida mas rica y divertida.
+        <br></br>
+        <br></br>
+        <br></br>
+        </Container>
+
+
+    );
+}
+export default DescripcionApp;
