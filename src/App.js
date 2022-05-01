902c7341f7054790e3861831d2e11bd8560ff884
Albert Rocha <escort84.ara@gmail.com>
2022-05-01 11:50:54 -0400

    Inicio de sesion

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+
+import React, {useState} from 'react';
 import {useContext} from 'react';
 import {UserContext} from './login/context/UserContext';
 import Login from './login/Login';
@@ -13,32 +14,38 @@ import PaginaInicio from './components/PaginaInicio';
 import Actividad from './actividades/Actividad';
 import Comunidad from './comunidad/publicacion';
 import Apoyo from './apoyo/Apoyo';
+import P404 from './Pagina404/P404';
 
 
 function App() {
 
   const {user} = useContext(UserContext); 
+  const [conectado, setConectado]= useState(true);
 
   return (
     <Router>
       <div className="App">
         <Routes>
-        {user && <Route exact path="/" element={<PaginaInicio />} />}
+          {user && <Route exact path="/" element={<PaginaInicio />} />}
           {!user && (
             <>
               <Route path="/login" element={<Login />} />
             </>
           )}
-          <Route path="*" element={<Navigate to={user ? '/' : '/login'} />} />
+
+          <Route path="/P404" element={<Navigate to={user ? '/' : '/Login'} />} />
 
           <Route exact path="/" element={<PaginaInicio />} />
           <Route exact path="/Comunidad" element={<Comunidad />} />
           <Route exact path="/Actividades" element={<Actividad />} />
           <Route exact path="/Voluntarios" element={<Apoyo />} />
+          <Route  path="*" element={<P404/>}/>
+
         </Routes>
 
       </div>
     </Router>
+   
   );
 }
 
--- src/login/Login.js
+++ src/login/Login.js
@@ -1,4 +1,5 @@
-import { useState, useContext } from 'react';
+import { useState, useContext} from 'react';
+import React, {useRef} from 'react';
 import { Link } from 'react-router-dom';
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
@@ -10,7 +11,28 @@ import Image from "react-bootstrap/Image";
 import { UserContext } from './context/UserContext';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSignInAlt } from "@fortawesome/free-solid-svg-icons";
-const Login = () => {
+
+
+const URL_LOGIN = "http://localhost:80/apiCam/login.php";
+
+const enviarDatos = async (url, datos)=> {
+    const resp = await fetch (url, {
+        method: 'POST',
+        body: JSON.stringify(datos),
+        headers:{
+            'Content-Type':'application/json'
+        }
+    });
+    console.log(resp);
+    const rjson = await resp.json();
+    console.log('hola');
+    console.log(rjson);
+
+    return rjson;
+}
+
+let i = 0;
+function Login(props) {
     const { loginUser, wait, loggedInCheck } = useContext(UserContext);
     const [redirect, setRedirect] = useState(false);
     const [errMsg, setErrMsg] = useState(false);
@@ -23,8 +45,8 @@ const Login = () => {
         setFormData({
             ...formData,
             [e.target.name]: e.target.value
-        })
-    }
+        });
+    };
 
     const submitForm = async (e) => {
         e.preventDefault();
@@ -42,7 +64,36 @@ const Login = () => {
             return;
         }
         setErrMsg(data.message);
-    }
+    };
+
+    const refUsuario = useRef(null);
+    const refContraseña = useRef(null);
+    
+
+    const handleLogin = async () => {
+
+        const datos = {
+            "usuario": refUsuario.current.value,
+            "clave": refContraseña.current.value
+        };
+
+        console.log(datos);
+        const respuestaJson = await enviarDatos (URL_LOGIN, datos);
+        console.log(respuestaJson.conectado);
+        console.log(i);
+
+        if(respuestaJson.conectado == true){
+            window.location.href = '/Comunidad';
+        }else{
+            i= i+1;
+            console.log(i);
+            if(i>3){
+                window.location.href = '*';
+                i=0
+            }
+        }
+        
+    };
 
     return (
         <div className='loginPage'>
@@ -59,23 +110,23 @@ const Login = () => {
                     <div className="mb-2 d-flex flex-row align-items-center">
                         <label htmlFor="email" className="col-sm-2 col-form-label">Email:</label>
                         <div class="col-sm-12 ">
-                            <input className="form-control center-block" type="email" name="email" onChange={onChangeInput} id="email" value={formData.email} required />
+                            <input className="form-control center-block" type="email" name="email" onChange={onChangeInput} id="email" value={formData.email} ref={refUsuario} required />
                         </div>
                     </div>
                     <div className="mb-2 d-flex flex-row align-items-center">
                         <label htmlFor="password" className="col-sm-2 col-form-label">Contraseña:</label>
                         <div class="col-sm-12">
-                            <input className="form-control center-block" type="password" name="password" onChange={onChangeInput} id="password" value={formData.password} required />
+                            <input className="form-control center-block" type="password" name="password" onChange={onChangeInput} id="password" value={formData.password} ref={refContraseña} required />
                         </div>
                     </div>
                     <i class='fas fa-sign-in-alt'></i>
                     {errMsg && <div className="err-msg">{errMsg}</div>}
                     <div className="d-flex flex-row align-items-center justify-content-center">
-                        {redirect ? redirect : <button type="submit" disabled={wait}><FontAwesomeIcon icon={faSignInAlt}/> Ingresar</button>}
+                        {redirect ? redirect : <button onClick={handleLogin} type="submit" disabled={wait}><FontAwesomeIcon icon={faSignInAlt}/> Ingresar</button>}
                     </div>
                 </form>
-            </Container >
-        </div >
+            </Container>
+        </div>
     );
 }
 
