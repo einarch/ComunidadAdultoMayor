2c123c61216a9785acdb11a31c633b4e4f69b5a9
rodrigo.gutierrez <rodrigo.gutierrez.acc@gmail.com>
2022-05-01 02:55:58 -0400

    Fixed responsive to card items in the sections

--- src/apoyo/Apoyo.js
+++ src/apoyo/Apoyo.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
 import axios from 'axios';
 import './../apoyo/Apoyo.css';
 import avatar from '../imagenes/avatar.jpg';
@@ -33,24 +34,25 @@ const Apoyo = ({ children }) => {
                 <br />
                 <h2 className="title"> Voluntarios de Apoyo</h2>
                 <br />
-                <br />
-                <Container id="containerSection" className="p-4 mb-4">
+                <Container className="p-4 mb-4">
                     {data.map(apoyo => {
                         return (
-                            <div id="itemSectionCard" className="card shadow-0 border rounded-3">
-                                <Container className="card-body">
-                                    <Container id="divItem" className="media d-flex centerVol">
-                                        <img src={avatar} alt="" className="rounded-circle" height="120" width="120"></img>
-                                        <div id="divBodyItem" className="media-body flex-grow-1">
+                            <Card id="cardItem" className="text-left">
+                                <Card.Body>
+                                    <Card.Text className='d-flex flex-row'>
+                                        <div className='col-sm-2 d-flex flex-column align-items-center justify-content-center '>
+                                            <img src={avatar} className="rounded-circle" height="120" width="120"></img>
+                                        </div>
+                                        <div className="col-sm-10 d-flex flex-column align-items-left justify-content-center ">
                                             <h3 className="cardItemUserName mt-0 mb-1"><b>{apoyo.nombre}</b></h3>
                                             <br></br>
                                             <h4 className="cardItemTitle">{apoyo.ciudad} &emsp; &ensp; <b>Teléfono:</b>{apoyo.telefono} </h4>
                                             <br></br>
                                             <h4 className="cardItemTitle"><b>Días Disponibles:</b> {apoyo.diasdisp}</h4>
                                         </div>
-                                    </Container>
-                                </Container>
-                            </div>
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
                         )
                     }
                     )}
