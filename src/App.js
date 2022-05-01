f24c766bb0011bce1188810443016990add42494
Albert Rocha <escort84.ara@gmail.com>
2022-05-01 14:09:45 -0400

    Correccion Inicio de sesion

--- src/App.js
+++ src/App.js
@@ -15,12 +15,8 @@ import PaginaInicio from './components/PaginaInicio';
 import Actividad from './actividades/Actividad';
 import Comunidad from './comunidad/publicacion';
 import Apoyo from './apoyo/Apoyo';
-<<<<<<< HEAD
 import P404 from './Pagina404/P404';
 
-=======
-import Header from './Header';
->>>>>>> 2c123c61216a9785acdb11a31c633b4e4f69b5a9
 
 function App() {
 
@@ -31,14 +27,7 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-<<<<<<< HEAD
           {user && <Route exact path="/" element={<PaginaInicio />} />}
-=======
-          
-
-
-        {user && <Route exact path="/" element={<PaginaInicio />} />}
->>>>>>> 2c123c61216a9785acdb11a31c633b4e4f69b5a9
           {!user && (
             <>
               <Route path="/login" element={<Login />} />
@@ -47,22 +36,12 @@ function App() {
 
           <Route path="/P404" element={<Navigate to={user ? '/' : '/Login'} />} />
 
-<<<<<<< HEAD
           <Route exact path="/" element={<PaginaInicio />} />
           <Route exact path="/Comunidad" element={<Comunidad />} />
           <Route exact path="/Actividades" element={<Actividad />} />
           <Route exact path="/Voluntarios" element={<Apoyo />} />
           <Route  path="*" element={<P404/>}/>
 
-=======
-          <Route path="/" element={<PaginaInicio />} />
-          <Route path="Home/*" element={<Header />} >
-            <Route index element={<Comunidad />} />
-            <Route path='comunidad' element={<Comunidad />} />
-            <Route path='actividades' element={<Actividad />} />
-            <Route path='voluntarios' element={<Apoyo />} />
-          </Route>
->>>>>>> 2c123c61216a9785acdb11a31c633b4e4f69b5a9
         </Routes>
 
       </div>
