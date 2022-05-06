14fc80741f4496ea487a5663f7aa805d8825d116
Albert Rocha <escort84.ara@gmail.com>
2022-05-05 22:23:53 -0400

    Alerta mejorada

--- src/login/Login.js
+++ src/login/Login.js
@@ -79,6 +79,7 @@ export const Login = () => {
     
     const [isValid, setIsValid] = useState(false);
     const handleLogin = async () => {
+        setIsValid(false)
         const datos = {
             "usuario": values.email,
             "clave": values.password
