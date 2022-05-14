4c9587bc28f989ad17036e068c1fb6196488f128
fabrizio.oropeza <201910093@est.umss.edu>
2022-05-14 19:31:13 -0400

    validaciones de voluntarios

--- src/apoyo/Apoyo.js
+++ src/apoyo/Apoyo.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 import axios from 'axios';
+import { Form } from 'react-bootstrap';
+import { useFormik, useField, useFormikContext } from "formik";
+import * as Yup from "yup";
 import './../apoyo/Apoyo.css';
 import avatar from '../imagenes/avatar.jpg';
 import configData from "../config/config.json";
@@ -19,7 +22,38 @@ const Apoyo = ({ children }) => {
                 console.log(error);
             })
     }
+    const { handleSubmit, handleChange, values, touched, errors, handleBlur } = useFormik({
 
+        initialValues: { telefono: "", dias: "", tipo: "", description: "" },
+        onSubmit: (values, { setSubmitting }, { resetForm }) => {
+            setTimeout(() => {
+                console.log("Logging in", values);
+                setSubmitting(false);
+            }, 500);
+
+            resetForm({ values: '' });
+        },
+
+        validationSchema: Yup.object().shape({
+            telefono: Yup.number()
+            .typeError("Solo se admiten numeros")
+                .required("Este campo es requerido")
+                .min(4000000, `minimo 7 digitos porfavor`)
+                .positive( `minimo positivos`),
+            dias: Yup.string()
+            .required("Este campo es requerido")
+            .min(4, "Dias de la semana porfavor")
+            .max(80, "Dias de la semana porfavor"),
+            tipo: Yup.string()
+                .required("Este campo es requerido")
+                .min(4, "El tipo de ayuda debe tener minimo 4 caracteres")
+                .max(255, "El tipo de ayuda debe tener máximo 255 caracteres"),
+            description: Yup.string()
+                .min(4, "El motivo debe tener minimo 4 caracteres")
+                .max(400, "El motivo debe tener máximo 400 caracteres"),
+
+        })
+    })
     useEffect(() => {
         peticionGet();
     }, [])
@@ -34,6 +68,105 @@ const Apoyo = ({ children }) => {
                 <br />
                 <h2 className="title"> Voluntarios de Apoyo</h2>
                 <br />
+                <Container className="d-flex flex-row justify-content-end">
+                    <button type="button" className="btn m-2 btn-primary" data-bs-toggle="modal" data-bs-target="#createActivity">Ser voluntario</button>
+                </Container>
+                <div align="center">
+                    <div className="modal fade" id="createActivity" tabIndex="-1" aria-hidden="true" aria-labelledby="modalTitle" data-bs-backdrop="static">
+                        <div className="modal-dialog modal-dialog-centered">
+                            <div className="modal-content">
+                                <div className="modalColor d-flex flex-row justify-content-center">
+                                    <h2 className="modal-title"><b>VOLUNTARIOS DE CORAZON</b></h2>
+                                </div>
+                                <div className="modal-body tam p-3 modalColor ">
+                                    <Form id="createActivityForm" className="row g-3" onSubmit={handleSubmit}>
+                                        <Form.Group className="col-md-12">
+                                            <Form.Label className="form-label textModal d-flex flex-row align-items-left">Telefono</Form.Label>
+                                            <Form.Control
+                                                type="text"
+                                                id="telefono"
+                                                name="telefono"
+                                                className={errors.telefono && touched.telefono && "error"}
+                                                class="form-control"
+                                                placeholder="Ingrese el nombre de la actividad"
+                                                onChange={handleChange}
+                                                onBlur={handleBlur}
+                                                value={values.telefono}
+                                            />
+                                            <Form.Text className="errorMessModal d-flex flex-row" muted>
+                                                {errors.telefono && touched.telefono && (
+                                                    <div className="input-feedback">{errors.telefono}</div>
+                                                )}
+                                            </Form.Text>
+                                        </Form.Group>
+                                        <Form.Group className="col-md-12">
+                                            <Form.Label className="form-label textModal d-flex flex-row align-items-left">Dias disponibles</Form.Label>
+                                            <Form.Control
+                                                type="text"
+                                                id="dias"
+                                                name="dias"
+                                                className={errors.dias && touched.dias && "error"}
+                                                class="form-control"
+                                                placeholder="Ingrese el nombre de la actividad"
+                                                onChange={handleChange}
+                                                onBlur={handleBlur}
+                                                value={values.dias}
+                                            />
+                                            <Form.Text className="errorMessModal d-flex flex-row" muted>
+                                                {errors.dias && touched.dias && (
+                                                    <div className="input-feedback">{errors.dias}</div>
+                                                )}
+                                            </Form.Text>
+                                        </Form.Group>
+                                        <Form.Group className="col-md-12">
+                                            <Form.Label className="form-label textModal d-flex flex-row align-items-left">Tipo de apoyo</Form.Label>
+                                            <Form.Control
+                                                type="text"
+                                                id="tipo"
+                                                name="tipo"
+                                                className={errors.tipo && touched.tipo && "error"}
+                                                class="form-control"
+                                                placeholder="Ingrese el nombre de la actividad"
+                                                onChange={handleChange}
+                                                onBlur={handleBlur}
+                                                value={values.tipo}
+                                            />
+                                            <Form.Text className="errorMessModal d-flex flex-row" muted>
+                                                {errors.tipo && touched.tipo && (
+                                                    <div className="input-feedback">{errors.tipo}</div>
+                                                )}
+                                            </Form.Text>
+                                        </Form.Group>
+                                        <Form.Group className="col-md-12">
+                                            <Form.Label className="form-label textModal d-flex flex-row align-items-left">Motivacion</Form.Label>
+                                            <Form.Control
+                                                as="textarea"
+                                                rows={3}
+                                                className={errors.description && touched.description && "error"}
+                                                class="form-control"
+                                                id="description"
+                                                name="description"
+                                                placeholder="Ingrese una descripción"
+                                                onChange={handleChange}
+                                                onBlur={handleBlur}
+                                                value={values.description}>
+                                            </Form.Control>
+                                            <Form.Text className="errorMessModal d-flex flex-row" muted>
+                                                {errors.description && touched.description && (
+                                                    <div className="input-feedback">{errors.description}</div>
+                                                )}
+                                            </Form.Text>
+                                        </Form.Group>
+                                    </Form>
+                                </div>
+                                <div className="model-footer col-12 modalColor" align="center">
+                                    <button type="button" class="btn btn-secondary col-3 m-2" data-bs-dismiss="modal">Cancelar</button>
+                                    <button type="submit" class="btn btn-success col-3 m-2" data-bs-dismiss="modal" >Crear</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <Container className="p-4 mb-4">
                     {data.map(apoyo => {
                         return (
